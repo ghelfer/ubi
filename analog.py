2a258ea724229531f62a5084d29e5bbba2ddb0db
ghelfer <ghelfer@gmail.com>
2022-06-22 18:37:26 -0300

    Update analog.py

--- analog.py
+++ analog.py
@@ -21,3 +21,23 @@ while True:
     print('Raw ADC Value: ', chan.value)
     print('ADC Voltage: ' + str(chan.voltage) + 'V')
     time.sleep(2)
+    
+
+ #  Define application commands and features:
+def _range(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+    
+def evaluateSensorValue():
+    # Test your module, then define the value range - in this case between 0 and 60000.
+    sensorValue = _range(channel_0.value, 0, 60000, 0, 1023)
+    sensor_value.value = sensorValue
+    # Threshold
+    if(sensorValue > 300):
+        status_text.value = "Status: DANGER"
+        status_text.text_color = "yellow"
+        warn("!!!DANGER!!!", "Air Quality Deteriorating!")
+    else:
+        status_text.value = "Status: OK"
+        status_text.text_color = "green"
+ 
+
